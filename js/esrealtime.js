2e70018f48405fc7ab261ebbb0bc5d734744da05
eag-ehsan <eag.ehsan@gmail.com>
2023-10-16 01:18:12 +0330

    hjuop

--- js/esrealtime.js
+++ js/esrealtime.js
@@ -10,6 +10,9 @@ var startDataIndex = 0;
 var endDataIndex = lenData;
 var sli1 = document.getElementById("sli1");
 var check2 = document.getElementById("check2");
+var startbtn = document.getElementById("startbtn");
+var timer = null;
+var timerstatus = true;
 function check2change() {
     if (check2.checked) {
         checkbool = true;
@@ -66,6 +69,26 @@ var options = {
             enabled: false
         }
     },
+    annotations: {
+        yaxis: [{
+            y: -1,
+            y2: 0,
+            borderColor: '#000',
+            fillColor: '#FE2019',
+            opacity: 0.3,
+            label: {
+              borderColor: '#FFFFFF',
+              style: {
+                fontSize: '10px',
+                color: '#333',
+                opacity:0.1,
+                
+              },
+              text: 'Sub Zero Values',
+            }
+        }
+        ]
+    },
     colors: ['#008FFB'],
         
     dataLabels: {
@@ -75,7 +98,7 @@ var options = {
         curve: 'smooth'
     },
     title: {
-        text: 'Dynamic Updating Chart',
+        text: 'Any Input streams can be save in a file or DB',
         align: 'left'
     },
     markers: {
@@ -96,14 +119,16 @@ var options = {
     legend: {
         show: false
     },
-};
+}; 
 
 var chart = new ApexCharts(document.querySelector("#chart"), options);
 
 chart.render();
 
 
-window.setInterval(function () {
+
+
+function forIntervaltimer() {
     //startDataIndex+=stepToNextIndex;
     //endDataIndex+=stepToNextIndex;
     shiftpushData();
@@ -120,5 +145,187 @@ window.setInterval(function () {
         data: ymarr
 
     }])
-}, 100)
+    starttimer();
+}
+function starttimer() {  // use a one-off timer
+    timer = setTimeout(forIntervaltimer, 100);
+};
+
+function stoptimer() {
+    clearTimeout(timer);
+};
+starttimer();
+function stopStream()
+{
+    if(timerstatus){
+        stoptimer();
+        timerstatus = false;
+        startbtn.innerHTML = "Start Again to gathering new Data";
+        startbtn.classList.remove("btn-warning");
+        startbtn.classList.add("btn-success");
+    }
+    else{
+        starttimer();
+        timerstatus = true;
+        startbtn.innerHTML = "Stop and analysis existing data";
+        startbtn.classList.remove("btn-success");
+        startbtn.classList.add("btn-warning");
+    }
+    updateChartOpt();
+}
+function updateChartOpt()
+{
+  
+    if(!timerstatus){
+        options = {
+            series: [{
+                data: ymarr
+            }],
+            chart: {
+                id: 'realtime',
+                height: 350,
+                type: 'line',
+                  
+                
+                toolbar: {
+                    show: true
+                },
+                zoom: {
+                    enabled: true
+                }
+            },
+            annotations: {
+                yaxis: [{
+                    y: -1,
+                    y2: 0,
+                    borderColor: '#000',
+                    fillColor: '#FE2019',
+                    opacity: 0.3,
+                    label: {
+                      borderColor: '#FFFFFF',
+                      style: {
+                        fontSize: '10px',
+                        color: '#333',
+                        opacity:0.1,
+                        
+                      },
+                      text: 'Sub Zero Values',
+                    }
+                }
+                ]
+            },
+            colors: ['#008FFB'],
+                
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                curve: 'smooth'
+            },
+            title: {
+                text: 'Any Input streams can be save in a file or DB',
+                align: 'left'
+            },
+            markers: {
+                size: 0
+            },
+            xaxis: {
+                type: 'numeric',
+                data: xmarr
+            },
+            yaxis: {
+        
+                decimalsInFloat: 2,
+        
+        
+                min: -1.1,
+                max: 1.1
+            },
+            legend: {
+                show: false
+            },
+        };
+
+    }
+    else{
+        options = {
+            series: [{
+                data: ymarr
+            }],
+            chart: {
+                id: 'realtime',
+                height: 350,
+                type: 'line',
+                  
+                animations: {
+                    enabled: true,
+                    easing: 'linear',
+                    dynamicAnimation: {
+                        speed: 100
+                    }
+                },
+                toolbar: {
+                    show: false
+                },
+                zoom: {
+                    enabled: false
+                }
+            },
+            annotations: {
+                yaxis: [{
+                    y: -1,
+                    y2: 0,
+                    borderColor: '#000',
+                    fillColor: '#FE2019',
+                    opacity: 0.3,
+                    label: {
+                      borderColor: '#FFFFFF',
+                      style: {
+                        fontSize: '10px',
+                        color: '#333',
+                        opacity:0.1,
+                        
+                      },
+                      text: 'Sub Zero Values',
+                    }
+                }
+                ]
+            },
+            colors: ['#008FFB'],
+                
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                curve: 'smooth'
+            },
+            title: {
+                text: 'Any Input streams can be save in a file or DB',
+                align: 'left'
+            },
+            markers: {
+                size: 0
+            },
+            xaxis: {
+                type: 'numeric',
+                data: xmarr
+            },
+            yaxis: {
+        
+                decimalsInFloat: 2,
+        
+        
+                min: -1.1,
+                max: 1.1
+            },
+            legend: {
+                show: false
+            },
+        }; 
+
+    }
+    chart.updateOptions(options);
+    
+}
+
 

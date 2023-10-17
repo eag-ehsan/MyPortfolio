65a0491b9e29152702cdd75011a65754bd54fd0d
eag-ehsan <eag.ehsan@gmail.com>
2023-10-18 01:02:35 +0330

    done for tonight

--- js/esmem.js
+++ js/esmem.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    var currentlvl = 1;
     var hightwidth = 100;
     const lvl1arr = [
         {
@@ -34,6 +35,247 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'straw1',
             img: 'images/sm-straw1.png'
         }
+    ]
+    const lvl2arr = [
+        {
+            name: 'aloo',
+            img: 'images/sm-aloo.png'
+        },
+        {
+            name: 'berry',
+            img: 'images/sm-berry.png'
+        },
+        {
+            name: 'lino2',
+            img: 'images/sm-lino2.png'
+        },
+        {
+            name: 'straw1',
+            img: 'images/sm-straw1.png'
+        },
+        {
+            name: 'aloo',
+            img: 'images/sm-aloo.png'
+        },
+        {
+            name: 'berry',
+            img: 'images/sm-berry.png'
+        },
+        {
+            name: 'lino2',
+            img: 'images/sm-lino2.png'
+        },
+        {
+            name: 'straw1',
+            img: 'images/sm-straw1.png'
+        },
+        {
+            name: 'cofee',
+            img: 'images/sm-cofee.png'
+        },
+        {
+            name: 'cofee',
+            img: 'images/sm-cofee.png'
+        },
+        {
+            name: 'hamb',
+            img: 'images/sm-hamb.png'
+        },
+        {
+            name: 'hamb',
+            img: 'images/sm-hamb.png'
+        },
+        {
+            name: 'iccr',
+            img: 'images/sm-iccr.png'
+        },
+        {
+            name: 'iccr',
+            img: 'images/sm-iccr.png'
+        },
+        {
+            name: 'kahoo',
+            img: 'images/sm-kahoo.png'
+        },
+        {
+            name: 'kahoo',
+            img: 'images/sm-kahoo.png'
+        },
+        {
+            name: 'limo',
+            img: 'images/sm-limo.png'
+        },
+        {
+            name: 'limo',
+            img: 'images/sm-limo.png'
+        },
+        {
+            name: 'mango',
+            img: 'images/sm-mango.png'
+        },
+        {
+            name: 'mango',
+            img: 'images/sm-mango.png'
+        }
+    ]
+    const lvl3arr = [
+        {
+            name: 'aloo',
+            img: 'images/sm-aloo.png'
+        },
+        {
+            name: 'berry',
+            img: 'images/sm-berry.png'
+        },
+        {
+            name: 'lino2',
+            img: 'images/sm-lino2.png'
+        },
+        {
+            name: 'straw1',
+            img: 'images/sm-straw1.png'
+        },
+        {
+            name: 'aloo',
+            img: 'images/sm-aloo.png'
+        },
+        {
+            name: 'berry',
+            img: 'images/sm-berry.png'
+        },
+        {
+            name: 'lino2',
+            img: 'images/sm-lino2.png'
+        },
+        {
+            name: 'straw1',
+            img: 'images/sm-straw1.png'
+        },
+        {
+            name: 'cofee',
+            img: 'images/sm-cofee.png'
+        },
+        {
+            name: 'cofee',
+            img: 'images/sm-cofee.png'
+        },
+        {
+            name: 'hamb',
+            img: 'images/sm-hamb.png'
+        },
+        {
+            name: 'hamb',
+            img: 'images/sm-hamb.png'
+        },
+        {
+            name: 'iccr',
+            img: 'images/sm-iccr.png'
+        },
+        {
+            name: 'iccr',
+            img: 'images/sm-iccr.png'
+        },
+        {
+            name: 'kahoo',
+            img: 'images/sm-kahoo.png'
+        },
+        {
+            name: 'kahoo',
+            img: 'images/sm-kahoo.png'
+        },
+        {
+            name: 'limo',
+            img: 'images/sm-limo.png'
+        },
+        {
+            name: 'limo',
+            img: 'images/sm-limo.png'
+        },
+        {
+            name: 'mango',
+            img: 'images/sm-mango.png'
+        },
+        {
+            name: 'orange',
+            img: 'images/sm-orange.png'
+        },
+        {
+            name: 'orange',
+            img: 'images/sm-orange.png'
+        },
+        {
+            name: 'pear',
+            img: 'images/sm-pear.png'
+        },
+        {
+            name: 'pear',
+            img: 'images/sm-pear.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/sm-pizza.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/sm-pizza.png'
+        },
+        {
+            name: 'pomeg',
+            img: 'images/sm-pomeg.png'
+        },
+        {
+            name: 'pomeg',
+            img: 'images/sm-pomeg.png'
+        },
+        {
+            name: 'cake',
+            img: 'images/sm-cake.png'
+        },
+        {
+            name: 'cake',
+            img: 'images/sm-cake.png'
+        },
+        {
+            name: 'eggp',
+            img: 'images/sm-eggp.png'
+        },
+        {
+            name: 'eggp',
+            img: 'images/sm-eggp.png'
+        },
+        {
+            name: 'java',
+            img: 'images/sm-java.png'
+        },
+        {
+            name: 'java',
+            img: 'images/sm-java.png'
+        },
+        {
+            name: 'tomat',
+            img: 'images/sm-tomat.png'
+        },
+        {
+            name: 'tomat',
+            img: 'images/sm-tomat.png'
+        },
+        {
+            name: 'grap',
+            img: 'images/sm-grap.png'
+        },
+        {
+            name: 'grap',
+            img: 'images/sm-grap.png'
+        },
+        {
+            name: 'angoo',
+            img: 'images/sm-angoo.png'
+        },
+        {
+            name: 'angoo',
+            img: 'images/sm-angoo.png'
+        }
+
     ]
     lvl1arr.sort(() => 0.5 - Math.random())
     const inttvv = document.getElementById('inttvv')
@@ -70,7 +312,7 @@ document.addEventListener('DOMContentLoaded', () => {
         
         for (let i = 0; i < alllightsnumber; i++)
         {
-            if(i<=numberoflights)
+            if(i<numberoflights)
             {
                 document.getElementById('imglight'+i).setAttribute('src','images/lighton.png')
             }
@@ -151,7 +393,176 @@ document.addEventListener('DOMContentLoaded', () => {
 
         }
     }
+    function lvl2create() {
+        
+        var itmp = 0;
+        var ltmp = 0;
+        var toptmp = '';
+        var lefttmp = '';
+        inttvv.setAttribute('style','width: 800px; height: 450px');
+        
+        
+        
+        onlights()
+        for (let i = 0; i < 20; i++) {
 
+            const divtile = document.createElement('div')
+            divtile.classList.add("tilessty")
+            divtile.classList.add("click")
+            divtile.setAttribute('id', 'divtile' + i)
+            divtile.setAttribute('data-id', i)
+            divtile.addEventListener('click', tilesflip2)
+            divtile.setAttribute('width', hightwidth)
+            divtile.setAttribute('height', hightwidth)
+            itmp = Math.floor(i/5)
+            ltmp = (hightwidth*i) - (itmp*hightwidth*5)
+            toptmp = 'top: ' + hightwidth*itmp + 'px';
+            lefttmp = 'left: ' + ltmp + 'px';
+            divtile.setAttribute('style', toptmp + '; ' + lefttmp)
+            
+            esmemlvl.appendChild(divtile)
+
+            const divfront = document.createElement('div')
+            const divback = document.createElement('div')
+            divfront.classList.add("front")
+            divback.classList.add("back")
+            divfront.setAttribute('id', 'frontid' + i)
+            divfront.setAttribute('data-id', i)
+            divback.setAttribute('id', 'backid' + i)
+            divback.setAttribute('data-id', i)
+
+
+
+            const divmom = document.querySelector('#divtile' + i)
+            divfront.setAttribute('width', hightwidth)
+            divfront.setAttribute('height', hightwidth)
+            divback.setAttribute('width', hightwidth)
+            divback.setAttribute('height', hightwidth)
+            divmom.appendChild(divfront)
+            divmom.appendChild(divback)
+
+
+            frontmom = document.querySelector('#frontid' + i)
+            backmom = document.querySelector('#backid' + i)
+
+            const tilefront = document.createElement('img')
+            const tileback = document.createElement('img')
+            tilefront.setAttribute('src', 'images/sm-blank.png')
+            tileback.setAttribute('src', lvl2arr[i].img)
+            tilefront.setAttribute('data-id', i)
+            tileback.setAttribute('data-id', i)
+            tilefront.setAttribute('width', hightwidth)
+            tilefront.setAttribute('height', hightwidth)
+            tileback.setAttribute('width', hightwidth)
+            tileback.setAttribute('height', hightwidth)
+            frontmom.appendChild(tilefront)
+            backmom.appendChild(tileback)
+
+
+
+
+
+        }
+    }
+    function lvl3create() {
+        
+        var itmp = 0;
+        var ltmp = 0;
+        var toptmp = '';
+        var lefttmp = '';
+        inttvv.setAttribute('style','width: 800px; height: 450px');
+        
+        
+        
+        onlights()
+        for (let i = 0; i < 40; i++) {
+
+            const divtile = document.createElement('div')
+            divtile.classList.add("tilessty")
+            divtile.classList.add("click")
+            divtile.setAttribute('id', 'divtile' + i)
+            divtile.setAttribute('data-id', i)
+            divtile.addEventListener('click', tilesflip3)
+            divtile.setAttribute('width', hightwidth)
+            divtile.setAttribute('height', hightwidth)
+            itmp = Math.floor(i/8)
+            ltmp = (hightwidth*i) - (itmp*hightwidth*8)
+            toptmp = 'top: ' + hightwidth*itmp + 'px';
+            lefttmp = 'left: ' + ltmp + 'px';
+            divtile.setAttribute('style', toptmp + '; ' + lefttmp)
+            
+            esmemlvl.appendChild(divtile)
+
+            const divfront = document.createElement('div')
+            const divback = document.createElement('div')
+            divfront.classList.add("front")
+            divback.classList.add("back")
+            divfront.setAttribute('id', 'frontid' + i)
+            divfront.setAttribute('data-id', i)
+            divback.setAttribute('id', 'backid' + i)
+            divback.setAttribute('data-id', i)
+
+
+
+            const divmom = document.querySelector('#divtile' + i)
+            divfront.setAttribute('width', hightwidth)
+            divfront.setAttribute('height', hightwidth)
+            divback.setAttribute('width', hightwidth)
+            divback.setAttribute('height', hightwidth)
+            divmom.appendChild(divfront)
+            divmom.appendChild(divback)
+
+
+            frontmom = document.querySelector('#frontid' + i)
+            backmom = document.querySelector('#backid' + i)
+
+            const tilefront = document.createElement('img')
+            const tileback = document.createElement('img')
+            tilefront.setAttribute('src', 'images/sm-blank.png')
+            tileback.setAttribute('src', lvl3arr[i].img)
+            tilefront.setAttribute('data-id', i)
+            tileback.setAttribute('data-id', i)
+            tilefront.setAttribute('width', hightwidth)
+            tilefront.setAttribute('height', hightwidth)
+            tileback.setAttribute('width', hightwidth)
+            tileback.setAttribute('height', hightwidth)
+            frontmom.appendChild(tilefront)
+            backmom.appendChild(tileback)
+
+
+
+
+
+        }
+    }
+    function tilesflip2() {
+        let idtile = this.getAttribute('data-id');
+        tiledelect.push(lvl2arr[idtile].name);
+        tiledelectId.push(idtile);
+        if (this.classList.contains("flip")) {
+            this.classList.remove("flip")
+        }
+        else {
+            this.classList.add("flip")
+        }
+        if (tiledelect.length === 2) {
+            setTimeout(checktiles, 500);
+        }
+    }
+    function tilesflip3() {
+        let idtile = this.getAttribute('data-id');
+        tiledelect.push(lvl3arr[idtile].name);
+        tiledelectId.push(idtile);
+        if (this.classList.contains("flip")) {
+            this.classList.remove("flip")
+        }
+        else {
+            this.classList.add("flip")
+        }
+        if (tiledelect.length === 2) {
+            setTimeout(checktiles, 500);
+        }
+    }
     function tilesflip() {
         let idtile = this.getAttribute('data-id');
         tiledelect.push(lvl1arr[idtile].name);
@@ -202,20 +613,86 @@ document.addEventListener('DOMContentLoaded', () => {
             }
 
         } 
-        if(numberoflights<=0)
-        {
-            urscore.innerHTML = 'Game Over. try again'
-        }
+        
         onlights()
         tiledelect = []
         tiledelectId = []
-        urscore.innerHTML = wintile.length
-        if (wintile.length === lvl1arr.length / 2) {
-            urscore.innerHTML = 'Nice Job, Level1 is done'
+        urscore.innerHTML = "Score:" + wintile.length + " Life: " +  numberoflights;
+        if(currentlvl==1)
+        {
+            if (wintile.length === lvl1arr.length / 2) {
+                urscore.innerHTML = 'Nice Job, Level1 is done'
+                gotolvl2();
+            }
+        }else if(currentlvl==2)
+        {
+            if (wintile.length === lvl2arr.length / 2) {
+                urscore.innerHTML = 'Nice Job, Level2 is done'
+                gotolvl3();
+            }
+        }else if(currentlvl==3)
+        {
+            if (wintile.length === lvl3arr.length / 2) {
+                urscore.innerHTML = 'Nice Job, Level3 is done'
+                
+            }
+        }
+        
+        if(numberoflights<=0)
+        {
+            resetgame();
+            
         }
     }
 
+    function resetgame()
+    {
+        alert("Game over. Game will be reset for another try")
+        urscore.innerHTML = 'Game Over. try again';
+        wintile = [];
+        while (esmemlvl.firstChild) {
+            esmemlvl.removeChild(esmemlvl.lastChild);
+          }
+          while (out2id.firstChild) {
+            out2id.removeChild(out2id.lastChild);
+          }
 
+
+        lvl1arr.sort(() => 0.5 - Math.random())
+        lvl1create();
+    }
+    function gotolvl2()
+    {
+        currentlvl = 2;
+        alert("Nice try, Level 2 is starting now. get ready")
+        urscore.innerHTML = "Score:" + wintile.length + " Life: " +  numberoflights
+        
+        while (esmemlvl.firstChild) {
+            esmemlvl.removeChild(esmemlvl.lastChild);
+          }
+          
+
+
+        lvl2arr.sort(() => 0.5 - Math.random())
+        lvl2create();
+
+    }
+    function gotolvl3()
+    {
+        currentlvl = 3;
+        alert("Nice try, Level 3 is starting now. get ready")
+        urscore.innerHTML = "Score:" + wintile.length + " Life: " +  numberoflights
+        
+        while (esmemlvl.firstChild) {
+            esmemlvl.removeChild(esmemlvl.lastChild);
+          }
+          
+
+
+        lvl3arr.sort(() => 0.5 - Math.random())
+        lvl3create();
+
+    }
 
     lvl1create();
 
